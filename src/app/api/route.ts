8170c32d7da0679eb2aa17d7196f0d635c69ed39
gongyexiangjin <717054907@qq.com>
2025-05-11 22:30:56 +0800

    feat: 增加后端逻辑

---
+++ src/app/api/route.ts
@@ -0,0 +1,240 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { 
+    DynamoDBDocumentClient, 
+    PutCommand, 
+    GetCommand,
+    UpdateCommand
+} from "@aws-sdk/lib-dynamodb";
+
+// 创建 DynamoDB 客户端
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION, // your-region 例如 "us-east-1"
+  credentials: {
+    accessKeyId: process.env.AWS_USER_ACCESS_KEY_ID || '', // your-access-key-id
+    secretAccessKey: process.env.AWS_USER_ACCESS_KEY_SECRET || '', // your-secret-access-key
+  }
+});
+
+// 创建文档客户端
+const docClient = DynamoDBDocumentClient.from(client);
+
+// 写入分数
+async function writeScore(player: string, score: number): Promise<void> {
+  try {
+    const oldScore = await readScore(player)
+    const command = new PutCommand({
+      TableName: "BlackJack",
+      Item: { player, score: oldScore + score }
+    });
+
+    await docClient.send(command);
+    console.log(`Successfully wrote score for player ${player}`);
+  } catch (error) {
+    console.error("Error writing score:", error);
+    throw error;
+  }
+}
+
+// 读取分数
+async function readScore(player: string): Promise<number> {
+  try {
+    const command = new GetCommand({
+        TableName: "BlackJack",
+        Key: { player }
+    });
+
+    const response = await docClient.send(command);
+    return response.Item?.score ?? 0;
+  } catch (error) {
+    console.error("Error reading score:", error);
+    throw error;
+  }
+}
+
+export interface Card {
+  rank: string,
+  suit: string,
+}
+
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const suits = ['♠️', '♥️', '♦️', '♣️']
+const initialCards = ranks.map(rank => suits.map(suit => ({ rank, suit }))).flat()
+
+// 游戏状态初始化值
+const gameState: {
+  playerHand: Card[], // 玩家手牌
+  dealerHand: Card[], // 庄家手牌
+  cards: Card[], // 全部手牌 52张牌
+  message: string, // 提示信息
+  score: number, // 得分
+} = {
+  playerHand: [],
+  dealerHand: [],
+  cards: initialCards,
+  message: '',
+  score: 0,
+}
+
+// 玩家地址
+const PLAYER_ADDRESS = 'player'
+
+// 叫牌抽卡的公用方法
+function getRandomCards(cards: Card[], count: number) {
+  const randomIndexSet = new Set<number>()
+  while (randomIndexSet.size < count) {
+    randomIndexSet.add(Math.floor(Math.random() * cards.length))
+  }
+  // 抽取的卡牌
+  const randomCards = cards.filter((_, index) => randomIndexSet.has(index))
+  // 抽取后剩余的卡牌
+  const remainingCards = cards.filter((_, index) => !randomIndexSet.has(index))
+  return [randomCards, remainingCards]
+}
+// 计算卡牌总点数的公用方法
+function calculateCardsTotal(cards: Card[]) {
+  let total = 0
+  let aceCount = 0
+  cards.forEach(card => {
+    if (card.rank === 'A') {
+      total += 11
+      aceCount++
+    } else if (['J', 'Q', 'K'].includes(card.rank)) {
+      total += 10
+    } else {
+      total += Number(card.rank)
+    }
+    while(total > 21 && aceCount > 0) {
+      total -= 10
+      aceCount--
+    }
+  })
+  return total
+}
+// 发送给前端数据的公用方法
+async function sendSuccessDataToFront() {
+  try {
+    // 读取分数
+    const score = await readScore(PLAYER_ADDRESS)
+    return new Response(JSON.stringify(
+      {
+        playerHand: gameState.playerHand,
+        // 游戏未结束时，玩家只能看到庄家的第一张牌
+        dealerHand: !gameState.message ? [gameState.dealerHand[0], { rank: '?', suit: '?'} as Card] : gameState.dealerHand,
+        message: gameState.message,
+        score,
+      }
+    ), {
+      status: 200,
+    })
+  } catch (error) {
+    console.error("写入分数出错:", error)
+    return sendErrorDataToFront('write score error.', 500)
+  }
+}
+// 发送给前端错误信息的公用方法
+function sendErrorDataToFront(message: string, status: number) {
+  return new Response(JSON.stringify({ message }), { status })
+}
+
+// 初始化获取卡牌
+export async function GET() {
+  // 重置游戏状态
+  gameState.playerHand = []
+  gameState.dealerHand = []
+  gameState.cards = initialCards
+  gameState.message = ''
+
+  // 初始化逻辑：
+  // 1、玩家随机获取两张牌
+  // 2、庄家随机获取两张牌
+  const [playerCards, remainingCards] = getRandomCards(gameState.cards, 2)
+  const [dealerCards, newCards] = getRandomCards(remainingCards, 2)
+  gameState.playerHand = playerCards
+  gameState.dealerHand = dealerCards
+  gameState.cards = newCards
+
+  try {
+    // 读取分数
+    const score = await readScore(PLAYER_ADDRESS)
+    console.log("当前分数:", score)
+    // 特殊处理，初始化获取的两张牌，可能为21点
+    const playerCardsTotal = calculateCardsTotal(gameState.playerHand)
+    const dealerCardsTotal = calculateCardsTotal(gameState.dealerHand)
+    if (playerCardsTotal === 21 && dealerCardsTotal === 21) {
+      gameState.message = 'draw'
+    } else if (playerCardsTotal === 21) {
+      await writeScore(PLAYER_ADDRESS, 100);
+      gameState.message = "Player win, Black jack"
+    } else if (dealerCardsTotal === 21) {
+      await writeScore(PLAYER_ADDRESS, -100);
+      gameState.message = "Player lose, Dealer black jack"
+    }
+    return sendSuccessDataToFront()
+  } catch (error) {
+    console.error("读取分数出错:", error)
+    return sendErrorDataToFront('read score error.', 500)
+  }
+}
+
+// 叫牌/停牌
+export async function POST(request: Request) {
+  const { action } = await request.json()
+  if (action === 'hit') {
+    console.log('进入叫牌逻辑')
+    // 玩家操作逻辑：
+    // 1、点击叫牌按钮，随机获取一张卡牌到玩家手牌中
+    // 2、判断玩家手牌总点数
+    // 2-1、如果玩家手牌大于21，提示信息：玩家失败，爆牌
+    // 2-2、如果玩家手牌等于21，提示信息：玩家胜利，21点
+    // 2-2、如果玩家手牌小于21，玩家可以继续叫牌或者停牌
+    const [randomCards, remainingCards] = getRandomCards(gameState.cards, 1)
+    gameState.playerHand.push(...randomCards)
+    gameState.cards = remainingCards
+    const playerCardsTotal = calculateCardsTotal(gameState.playerHand)
+    if (playerCardsTotal > 21) {
+      gameState.message = "Player lose, Bust"
+      gameState.score -= 100
+    } else if (playerCardsTotal === 21) {
+      gameState.message = "Player win, Black jack"
+      gameState.score += 100
+    }
+  } else if (action === 'stand') {
+    console.log('进入停牌逻辑')
+    // 庄家操作逻辑：
+    // 1、玩家点击停牌时，随机获取一张卡牌到庄家手牌中
+    // 2、持续获取卡牌到庄家手牌，庄家手牌总点数为17或者大于17时停止
+    // 3、如果庄家手牌大于21，玩家胜利，提示信息：庄家爆牌
+    // 4、如果庄家手牌等于21，玩家失败，提示信息：庄家21点
+    // 5、如果庄家手牌小于21
+    // 5-1、如果庄家手牌总点数大于玩家手牌，玩家失败，提示信息：玩家失败
+    // 5-2、如果庄家手牌总点数小于玩家手牌，玩家胜利，提示信息：玩家胜利
+    // 5-3、如果庄家手牌总点数等于玩家手牌，平牌，提示信息：平牌
+    while(calculateCardsTotal(gameState.dealerHand) < 17) {
+      const [randomCards, remainingCards] = getRandomCards(gameState.cards, 1)
+      gameState.dealerHand.push(...randomCards)
+      gameState.cards = remainingCards
+    }
+    const dealerCardsTotal = calculateCardsTotal(gameState.dealerHand)
+    if (dealerCardsTotal > 21) {
+      await writeScore(PLAYER_ADDRESS, 100)
+      gameState.message = "Player win, Dealer bust"
+    } else if (dealerCardsTotal === 21) {
+      await writeScore(PLAYER_ADDRESS, -100)
+      gameState.message = "Player lose, Dealer black jack"
+    } else {
+      const playerCardsTotal = calculateCardsTotal(gameState.playerHand)
+      if (dealerCardsTotal > playerCardsTotal) {
+        await writeScore(PLAYER_ADDRESS, -100)
+        gameState.message = "Player lose"
+      } else if (dealerCardsTotal < playerCardsTotal) {
+        await writeScore(PLAYER_ADDRESS, 100)
+        gameState.message = "Player win"
+      } else {
+        gameState.message = "draw"
+      }
+    }
+  } else {
+    return sendErrorDataToFront('Invalid action.', 400)
+  }
+  return sendSuccessDataToFront()
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,65 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-  const suits = ['♠️', '♥️', '♦️', '♣️']
-  const initDeck = ranks.map(rank => suits.map(suit => ({ rank, suit }))).flat()
-
-  const [ deck, setDeck ] = useState<{ rank: string, suit: string }[]>([])
-  const [winner, setWinner] = useState('')
+  const [score, setScore] = useState(0)
   const [message, setMessage] = useState('')
+  const [playerHand, setPlayerHand] = useState<{ rank: string, suit: string }[]>([])
+  const [dealerHand, setDealerHand] = useState<{ rank: string, suit: string }[]>([])
 
   useEffect(() => {
-    setDeck(initDeck)
-    setWinner('player1')
-    setMessage('Black Jack')
+    handleReset()
   }, [])
+
+  // 点击叫牌按钮
+  async function handleHit() {
+    const response = await fetch('/api', {
+      method: 'POST',
+      body: JSON.stringify({ action: 'hit' }),
+    })
+    if (response.status != 200) return
+    const { playerHand, message, score } = await response.json()
+    console.log('叫牌===>', playerHand)
+    setPlayerHand(playerHand)
+    setMessage(message)
+    setScore(score)
+  }
+  // 点击停牌按钮
+  async function handleStand() {
+    const response = await fetch('/api', {
+      method: 'POST',
+      body: JSON.stringify({ action: 'stand' }),
+    })
+    if (response.status != 200) return
+    const { dealerHand, message, score } = await response.json()
+    console.log('停牌===>', dealerHand)
+    setDealerHand(dealerHand)
+    setMessage(message)
+    setScore(score)
+  }
+  // 点击重置按钮
+  async function handleReset() {
+    const response = await fetch('/api', { method: 'GET' })
+    console.log('response===>', response)
+    if (response.status != 200) return
+    const { playerHand, dealerHand, message, score } = await response.json()
+    setPlayerHand(playerHand)
+    setDealerHand(dealerHand)
+    setMessage(message)
+    setScore(score)
+  }
 
   return (
     <div className="flex flex-col gap-2 items-center justify-center h-screen bg-gray-300">
       <h1 className="text-3xl blod">Web3 Black Jack</h1>
       <h2 
-        className={`text-2xl blod ${winner === 'player' ? 'bg-green-300' : 'bg-blue-300'}`}
-      >{ message }</h2>
+        className={`text-2xl blod ${message.includes('win') ? 'bg-green-300' : 'bg-blue-300'}`}
+      >Score: {score} { message }</h2>
 
       <div className="mt-4">
         <h2>Dealer's hand</h2>
         <div className="flex flex-row gap-2">
           {
-            deck.slice(0, 4).map((card, index) => (
+            (dealerHand || []).map((card, index) => (
               <div key={index} className="w-32 h-42 border-1 border-black bg-white rounded-md flex flex-col justify-between">
                 <div className="self-start p-2 text-lg">{card.rank}</div>
                 <div className="self-center p-2 text-3xl">{card.suit}</div>
@@ -42,7 +75,7 @@ export default function App() {
         <h2>Player's hand</h2>
         <div className="flex flex-row gap-2">
           {
-            deck.slice(0, 4).map((card, index) => (
+            (playerHand || []).map((card, index) => (
               <div key={index} className="w-32 h-42 border-1 border-black bg-white rounded-md flex flex-col justify-between">
                 <div className="self-start p-2 text-lg">{card.rank}</div>
                 <div className="self-center p-2 text-3xl">{card.suit}</div>
@@ -54,9 +87,14 @@ export default function App() {
       </div>
 
       <div className="flex flex-row gap-2 mt-4">
-        <button className="bg-blue-300 rounded-md p-2">Hit</button>
-        <button className="bg-blue-300 rounded-md p-2">Stand</button>
-        <button className="bg-blue-300 rounded-md p-2">Reset</button>
+        { 
+          message === '' ?
+            <>
+              <button className="bg-blue-300 rounded-md p-2" onClick={handleHit}>Hit</button>
+              <button className="bg-blue-300 rounded-md p-2" onClick={handleStand}>Stand</button>
+            </> :
+            <button className="bg-blue-300 rounded-md p-2" onClick={handleReset}>Reset</button>
+        }
       </div>
     </div>
   )
